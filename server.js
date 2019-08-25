3b0363461c8ea93f95349e00d4da3b65a271b27f
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2019-08-25 15:58:32 -0300

    add listenning message

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ res.sendFile(path.join(__dirname+'/dist/customer-list/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT, ()=> console.log(`App listenning to port ${process.env.PORT}`));
