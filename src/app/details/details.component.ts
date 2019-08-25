00b125847c4f26c309c63622c3cc2fe2badcaf7c
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2019-08-25 14:24:55 -0300

    add details component

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { CustomersList } from '../app.models';
+import { AppService } from '../app.service';
+import { Router } from '@angular/router';
+import { DetailsService } from './details.service';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.sass'],
+})
+export class DetailsComponent implements OnInit {
+  customerData: CustomersList;
+
+  constructor(
+    private appService: AppService,
+    private detailsService: DetailsService,
+    private router: Router
+    ) { }
+
+  onClick() {
+    this.detailsService.editCustomers(this.customerData);
+  }
+
+  onChange(event, key) {
+    this.customerData[key] = event.target.value;
+  }
+
+  ngOnInit() {
+    this.customerData = this.appService.getCustomer();
+    if (Number.isNaN(this.customerData.id)) {
+      this.router.navigate(['/']);
+    }
+  }
+
+
+}
