2aa6bfc9727502060e6c1dd3d852f49ce1004e07
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2019-08-25 14:25:42 -0300

    add notifications component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit, HostListener } from '@angular/core';
-import {CustomersList} from '../app.models';
+import { Component, OnInit, HostListener, AfterViewInit} from '@angular/core';
+import { CustomersList } from '../app.models';
 import { HomeService } from './home.service';
+import { AppService } from '../app.service';
+import { Router } from '@angular/router';
+import { DetailsService } from '../details/details.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   pageWidth = NaN;
   displayedColumns: string[] = ['id', 'name', 'age', 'city'];
   dataSource: CustomersList[];
+  showNotification = false;
+  notification = '';
+
+  constructor(
+    private homeService: HomeService,
+    private appService: AppService,
+    private detailsService: DetailsService,
+    private router: Router
+    )  { }
 
   onClick(element) {
     this.appService.retrieveCustomer(element);
@@ -23,9 +35,18 @@ export class HomeComponent implements OnInit {
   }
   ngOnInit() {
     this.pageWidth = window.innerWidth;
-    return this.homeService.getCustomers()
+    this.homeService.getCustomers()
     .subscribe( customers => this.dataSource = customers);
+    this.notification = this.detailsService.sendNotification().message;
+    if (this.detailsService.sendNotification().showNotification) {
+    setTimeout(() =>  this.showNotification = this.detailsService.sendNotification().showNotification, 100);
+    }
   }
+
+  ngAfterViewInit() {
+    setTimeout(() => this.showNotification = false, 3000);
+  }
+
 
 }
 
